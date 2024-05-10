91539dde17d21b6ca7a42c37c8cebfd45692ed79
IliaWithHat <rozhkoilia5533@gmail.com>
2024-05-10 11:08:23 +0300

    add crud

--- licensing-service/src/main/java/org/ilia/licensingservice/controller/LicenseController.java
+++ licensing-service/src/main/java/org/ilia/licensingservice/controller/LicenseController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.ilia.licensingservice.entity.License;
 import org.ilia.licensingservice.service.LicenseService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
@@ -19,7 +16,24 @@ public class LicenseController {
     @GetMapping("/{licenseId}")
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
-        License license = licenseService.getLicense(licenseId, organizationId);
-        return ResponseEntity.ok(license);
+        return ResponseEntity.ok(licenseService.getLicense(licenseId, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @DeleteMapping("/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
+                                                @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
     }
 }
