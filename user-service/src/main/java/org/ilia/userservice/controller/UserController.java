89d38a000dc41dd3927cbfb52ad1b3c792b6858b
IliaWithHat <rozhkoilia5533@gmail.com>
2024-06-04 15:48:03 +0300

    Add prefix to an url path

--- user-service/src/main/java/org/ilia/userservice/controller/UserController.java
+++ user-service/src/main/java/org/ilia/userservice/controller/UserController.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
